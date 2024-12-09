e560f77834b3bd3dc5b18f2df402ebe49c600aff
= <=>
2024-12-09 16:04:54 +0200

    structure

---
+++ globals.py
@@ -0,0 +1,11 @@
+class Globals:
+    project_directory = ""
+
+
+    def __init__(self):
+        self.db_manager = ""  # some class
+
+class Models:
+
+    def __init__(self):
+        pass
---
+++ rag/model_api.py
@@ -0,0 +1,7 @@
+from globals import Globals
+
+
+class ModelApi:
+
+    def __init__(self):
+        pass
